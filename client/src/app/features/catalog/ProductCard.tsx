ca4a148c851b7e7e5c087f326d704f5545b00625
meetj0806 <meet2003j@gmail.com>
2026-08-19 07:35:22 +0530

    Add ProductDetails functionality

--- client/src/app/features/catalog/ProductCard.tsx
+++ client/src/app/features/catalog/ProductCard.tsx
@@ -7,6 +7,7 @@ import {
   Typography,
 } from "@mui/material";
 import type { Product } from "../../models/product";
+import { Link } from "react-router-dom";
 
 type Props = {
   product: Product;
@@ -45,7 +46,7 @@ export default function ProductCard({ product }: Props) {
 
       <CardActions sx={{ justifyContent: "space-between" }}>
         <Button>Add to Cart</Button>
-        <Button>View</Button>
+        <Button component={Link} to={`/catalog/${product.id}`}>View</Button>
       </CardActions>
     </Card>
   );
